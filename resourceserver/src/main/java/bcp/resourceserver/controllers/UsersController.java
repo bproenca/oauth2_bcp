353cc34b9e86cddbb812011f213760ba35244323
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-02-18 20:57:13 -0300

    PreAuthorize

--- resourceserver/src/main/java/bcp/resourceserver/controllers/UsersController.java
+++ resourceserver/src/main/java/bcp/resourceserver/controllers/UsersController.java
@@ -2,7 +2,14 @@ package bcp.resourceserver.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +25,15 @@ public class UsersController {
         return "Working on port: " + env.getProperty("local.server.port");
     }
 
+    @Secured("ROLE_developer")
+    @PutMapping(path = "/{id}")
+    public String updateUser(@PathVariable String id) {
+        return "Updated user with id " + id;
+    }
+    
+    @PreAuthorize("hasAuthority('ROLE_developer') and #id == #jwt.subject")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+    }
 }
