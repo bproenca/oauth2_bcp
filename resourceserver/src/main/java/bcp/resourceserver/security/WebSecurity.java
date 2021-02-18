2f2509bfcfe2b5fd5100de4b2456c6d642a72646
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-02-17 22:50:35 -0300

    Scope and Role Based Access Control with KeyCloak

---
+++ resourceserver/src/main/java/bcp/resourceserver/security/WebSecurity.java
@@ -0,0 +1,32 @@
+package bcp.resourceserver.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        http// .cors().and()
+                .authorizeRequests()
+                        .antMatchers(HttpMethod.GET, "/users/status/check")
+                        //.hasAuthority("SCOPE_profile") //client scopes (default or requested)
+                        .hasRole("developer") // user role mappings
+                        // .hasAnyAuthority("ROLE_developer") // same as line above
+                        // .hasAnyRole("devleoper","user")
+                    .anyRequest().authenticated()
+                    .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter)
+            ;
+    }
+}
