7765e79942698647197a9f4549675feb8efa37a1
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-03-23 19:49:26 -0300

    remote realm management

---
+++ remote-manage-realms/src/test/java/bcp/remotemanagerealms/RemoteManageRealmsApplicationTests.java
@@ -0,0 +1,13 @@
+package bcp.remotemanagerealms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteManageRealmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
